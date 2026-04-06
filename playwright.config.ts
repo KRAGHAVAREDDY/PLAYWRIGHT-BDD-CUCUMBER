c232ee7a5a0a293f803b9d295ea67142e8223947
KRAGHAVAREDDY <venkataraghavan359@gmail.com>
2026-04-06 23:35:24 +0530

    added trace vedio and screenshot

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,10 +34,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "http://localhost:3000",
-
+    // baseURL: "http://localhost:3000",
+    screenshot: "on",
+    trace: "on",
+    video: "on",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+   
   },
 
   /* Configure projects for major browsers */
